665265e8d2ce04d38d4c5112fc38a778d73c008c
Roy Millan <60663116+RoyMillan96@users.noreply.github.com>
2023-10-19 07:48:21 -0600

    add requets to cli

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import typer
+import requests
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
@@ -45,6 +46,29 @@ def listar_items():
     typer.echo("Ítems en la base de datos:")
     for item in items:
         typer.echo(f"ID: {item.id}, Nombre: {item.name}")
+
+def fetch_data(name_city: str):
+    # URL del endpoint externo que deseas consultar
+    api_key = 'apikey'
+    url = 'url' + 'locations/v1/cities/search'
+
+    params = {
+            'q': name_city,
+            'apikey': api_key
+        }
+    try:
+        # Realiza la solicitud GET al endpoint
+        response = requests.get(url, params=params)
+
+        # Verifica si la solicitud fue exitosa (código de estado HTTP 200)
+        if response.ok:
+            data = response.json()
+            print("la data es: ", data)
+        else:
+            print(f'La solicitud no fue exitosa. Código de estado: {response.status_code}')
+    except Exception as error:
+        print(f'La solicitud no fue exitosa. Código de estado: 500')
+
 
 if __name__ == "__main__":
     app()
